bd02214bc0cbfaca6165e89fa4283575c455d447
sageb18 <taddeosage@gmail.com>
2026-07-11 21:11:12 -0700

    base models for project initialization

---
+++ src/main/java/com/sageb18/covered/CoveredApplication.java
@@ -0,0 +1,13 @@
+package com.sageb18.covered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoveredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoveredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sageb18/covered/CoveredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sageb18.covered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoveredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
