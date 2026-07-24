31086fb089c5ff791193b259e6688f1f4a5f1891
sageb18 <taddeosage@gmail.com>
2026-07-23 21:15:25 -0700

    built ShiftAssignment model using appropriate Timefold annotations, started UnavailabilityWindow model for employee availability. Need to build the constraint provider.

--- src/main/java/com/sageb18/covered/model/ShiftAssignment.java
+++ src/main/java/com/sageb18/covered/model/ShiftAssignment.java
@@ -1,14 +1,48 @@
 package com.sageb18.covered.model;
 
-import jakarta.persistence.*;
+import ai.timefold.solver.core.api.domain.entity.PlanningEntity;
+import ai.timefold.solver.core.api.domain.lookup.PlanningId;
+import ai.timefold.solver.core.api.domain.variable.PlanningVariable;
 
 import java.util.UUID;
 
-@Entity
-@Table(name = "ShiftAssignment")
+@PlanningEntity
 public class ShiftAssignment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @PlanningId
     private UUID id;
+
+    private Shift shift;
+
+    @PlanningVariable
+    private Employee employee;
+
+    public ShiftAssignment() {
+    }
+
+    public ShiftAssignment(UUID id, Shift shift) {
+        this.id = id;
+        this.shift = shift;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return shift + " -> " + (employee == null ? "UNASSIGNED" : employee);
+    }
 }
--- src/main/java/com/sageb18/covered/model/UnavailabilityWindow.java
+++ src/main/java/com/sageb18/covered/model/UnavailabilityWindow.java
@@ -2,6 +2,7 @@ package com.sageb18.covered.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -15,6 +16,14 @@ public class UnavailabilityWindow {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
+
+    @Column(nullable = false)
+    private LocalDateTime UnavailabilityStartTime;
+
+    @Column(nullable = false)
+    private LocalDateTime UnavailabilityEndTime;
+
+
 
 
 }
