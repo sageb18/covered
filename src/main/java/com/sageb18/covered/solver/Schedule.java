a0d7efc840b105fe64f29897a0688d93ac7012f0
sageb18 <taddeosage@gmail.com>
2026-07-27 23:49:09 -0700

    Started ScheduleConstraintProvider and finished the requiredSkill constraint. Schedule class created.

---
+++ src/main/java/com/sageb18/covered/solver/Schedule.java
@@ -0,0 +1,13 @@
+package com.sageb18.covered.solver;
+
+import ai.timefold.solver.core.api.domain.solution.PlanningSolution;
+import com.sageb18.covered.model.Employee;
+
+import java.util.List;
+
+@PlanningSolution
+public class Schedule {
+
+
+    private List<Employee> employee;
+}
--- src/main/java/com/sageb18/covered/solver/ScheduleConstraintProvider.java
+++ src/main/java/com/sageb18/covered/solver/ScheduleConstraintProvider.java
@@ -1,4 +1,30 @@
 package com.sageb18.covered.solver;
 
-public class ScheduleConstraintProvider {
+import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
+import ai.timefold.solver.core.api.score.stream.Constraint;
+import ai.timefold.solver.core.api.score.stream.ConstraintFactory;
+import ai.timefold.solver.core.api.score.stream.ConstraintProvider;
+import com.sageb18.covered.model.ShiftAssignment;
+
+public class ScheduleConstraintProvider implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory factory) {
+        return new Constraint[] {
+                requiredSkill(factory)
+        };
+    }
+
+    // Constraint rule #1 for employees
+    // Nobody should be assigned a shift they lack the skill for.
+    private Constraint requiredSkill(ConstraintFactory factory) {
+        return factory.forEach(ShiftAssignment.class)
+                .filter(assignment ->
+                        assignment.getEmployee() != null &&
+                        !assignment.getEmployee().getSkills().contains(assignment.getShift().getRequiredSkill()))
+                // ^^^ Lambda that runs once per assignment, returns a bool
+                // "true" = rule is broken, "false" = rule not broken
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("Missing required skill");
+    }
 }
