e4ef652f7f93f8134d89f25d1d300d43631a3702
sageb18 <taddeosage@gmail.com>
2026-08-04 20:36:41 -0700

    Created a fake POST in ScheduleController to test the endpoint. Works.

--- src/main/java/com/sageb18/covered/controller/ScheduleController.java
+++ src/main/java/com/sageb18/covered/controller/ScheduleController.java
@@ -1,5 +1,7 @@
 package com.sageb18.covered.controller;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
 
     @PostMapping("/solve")
-    public String solve() {
-        return "solve endpoint is working";
+    public Map<String, Object> solve() {
+        return Map.of(
+                "status", "FEASIBLE",
+                "assignments", List.of(
+                        Map.of("shift", "Monday AM", "employee", "Alex"),
+                        Map.of("shift", "Monday PM", "employee", "Sam")
+                )
+        );
     }
 }
